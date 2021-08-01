485c086219b95924a8a6f01a31d4b39ca87986b8
ekdeguzm <35510088+ekdeguzm@users.noreply.github.com>
2021-07-31 20:50:02 -0700

    Add files via upload

---
+++ pong.py
@@ -0,0 +1,146 @@
+# Simple pong in python 3 for Beginners
+# 
+
+import turtle
+import os
+
+
+wn = turtle.Screen()
+wn.title("Pong by Kawayan")
+wn.bgcolor("black")
+wn.setup(width=900, height=600)
+wn.tracer(0)
+
+# Score
+score_a = 0
+score_b = 0
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-400, 0)
+
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(400, 0)
+
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 1.5
+ball.dy = 1.5
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0    Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+
+# Function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+# Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+
+# Main game loop
+
+while True:
+    wn.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 460:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}    Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
+    if score_b == 3:
+        pen = turtle.Turtle()
+        pen.speed(0)
+        pen.color("white")
+        pen.penup()
+        pen.hideturtle()
+        pen.goto(0, 0)
+        pen.write("You WIN Player B!", align="center", font=("Courier", 30, "normal"))
+
+    if ball.xcor() < -460:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}    Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
+    if score_a == 3:
+        pen = turtle.Turtle()
+        pen.speed(0)
+        pen.color("white")
+        pen.penup()
+        pen.hideturtle()
+        pen.goto(0, 0)
+        pen.write("You WIN Player A!", align="center", font=("Courier", 30, "normal"))
+
+    # Paddle and ball collisions
+    if (ball.xcor() > 390 and ball.xcor() < 400) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(390)
+        ball.dx *= -1
+        os.system("afplay splat_sound.wav&")
+
+    if (ball.xcor() < -390 and ball.xcor() < -400) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-390)
+        ball.dx *= -1
+        os.system("afplay splat_sound.wav&")
